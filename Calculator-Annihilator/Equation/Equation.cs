64ab81c22be68ef924f5ef9396034a76519badf4
Ungouge <zlobik89@gmail.com>
2018-10-29 22:27:33 +0100

    Created Equation.Values and separition of Values from Equation

--- Calculator-Annihilator/Equation/Equation.cs
+++ Calculator-Annihilator/Equation/Equation.cs
@@ -9,32 +9,6 @@ namespace Calculator_Annihilator
 {
 	public partial class Equation
 	{
-		public string the_Equation;
-
-		private Numerical_System _Numerical_System;
-
-		private Parser _Parser = new Parser();
-		private Calc _Calc = new Calc();
-
-		private Equation_Elements Elements_Colection = new Equation_Elements();
-
-		private bool Does_Solved = false;
-
-		public double Solved_Value
-		{
-			get
-			{
-				if (Does_Solved == false)
-					Solver();
-
-				return Solved_Value;
-			}
-			private set
-			{
-				Solved_Value = value;
-			}
-		}
-
 		public Equation ( string eq, Numerical_System ns )
 		{
 			try
---
+++ Calculator-Annihilator/Equation/Values.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Annihilator
+{
+	partial class Equation
+	{
+		public string the_Equation;
+
+		private Numerical_System _Numerical_System;
+
+		private Parser _Parser = new Parser();
+		private Calc _Calc = new Calc();
+
+		private Equation_Elements Elements_Colection = new Equation_Elements();
+
+		private bool Does_Solved = false;
+
+		public double Solved_Value
+		{
+			get
+			{
+				if (Does_Solved == false)
+					Solver();
+
+				return Solved_Value;
+			}
+			private set
+			{
+				Solved_Value = value;
+			}
+		}
+	}
+}
