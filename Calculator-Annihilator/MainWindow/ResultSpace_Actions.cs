6f9cee7b3cb4b700f10a2ee6cfe4f54ba448fe18
Ungouge <zlobik89@gmail.com>
2018-10-31 14:24:14 +0100

    Added ResultSpcace_Actions

---
+++ Calculator-Annihilator/MainWindow/ResultSpace_Actions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Annihilator
+{
+	public partial class MainWindow
+	{
+		private void Reset_ResultSpace_Text(string new_ResultSpace_Text)
+		{
+			ResultSpace.Clear();
+			ResultSpace.Text = new_ResultSpace_Text;
+		}
+
+		private void Insert_To_ResultSpace(char _char)
+		{
+			Insert_To_ResultSpace(_char.ToString());
+		}
+
+		private void Insert_To_ResultSpace(string _string)
+		{
+			ResultSpace.Text = ResultSpace.Text.Insert(ResultSpace.Text.Length, _string);
+		}
+
+		private void Clear_ResultSpace()
+		{
+			ResultSpace.Clear();
+		}
+	}
+}
