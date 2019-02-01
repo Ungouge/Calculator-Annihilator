938c5f458e1a4b668a029cddbd502832a9f6420e
Ungouge <zlobik89@gmail.com>
2019-02-01 17:05:37 +0100

    Added File in menu bar
    * with 3 buttons with own click events.

---
+++ Calculator-Annihilator/MainWindow/Button_Click events/Menu_Bar_Buttons/File/New_WindowButton_Click.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace Calculator_Annihilator
+{
+    partial class MainWindow
+    {
+        private void New_WindowButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Calculator-Annihilator/MainWindow/Button_Click events/Menu_Bar_Buttons/File/OptionsButton_Click.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace Calculator_Annihilator
+{
+    partial class MainWindow
+    {
+        private void OptionsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
