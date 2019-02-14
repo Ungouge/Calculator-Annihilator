4ef48ba815f3395dccb2c60cbd7ebff42b8c346c
Ungouge <zlobik89@gmail.com>
2019-02-14 22:47:03 +0100

    Added values in Options_Window for cleaner code.

---
+++ Calculator-Annihilator/Options_Window/values.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calculator_Annihilator
+{
+    partial class Options_Window : Window, IMainWindow_Children
+    {
+        internal Bindable_Resources _Bindable_Resources;
+
+        public MainWindow Parent_MainWindow { get; set; }
+
+        private Options Options;
+
+        List<Action<MainWindow>> Options_Changed_List;
+    }
+}
