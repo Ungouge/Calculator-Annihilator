f8e14f098cd21ff4c913dbf4fe922ff6c0bfccc4
Ungouge <zlobik89@gmail.com>
2019-02-15 11:54:28 +0100

    Added  Change_Language_Option_Command as implementation of IChange_Option_Command.

---
+++ Calculator-Annihilator/IChange_Option_Command/Change_Language_Option_Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator_Annihilator
+{
+    /// <summary>
+    /// Changes language in Options class.
+    /// </summary>
+    internal class Change_Language_Option_Command : IChange_Option_Command
+    {
+        private readonly Languages Language_To_Change;
+
+        /// <summary>
+        /// Constructs new command for changing language in Options class.
+        /// </summary>
+        internal Change_Language_Option_Command(Languages language_To_Change, bool in_All_Instances)
+        {
+            Language_To_Change = language_To_Change;
+        }
+
+        /// <summary>
+        /// Change language option in given Options class
+        /// </summary>
+        public void Change_Option(Options options)
+        {
+            options.Set_Language(Language_To_Change);
+        }
+    }
+}
