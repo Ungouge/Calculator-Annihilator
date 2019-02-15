59364593e31c05a8a47d01a679d05ca962b23324
Ungouge <zlobik89@gmail.com>
2019-02-15 11:55:14 +0100

    Clering code.

--- Calculator-Annihilator/IChange_Option_Command/IChange_Option_Command.cs
+++ Calculator-Annihilator/IChange_Option_Command/IChange_Option_Command.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Calculator_Annihilator
 {
-
+    /// <summary>
+    /// Provides Machanism to change option in Options class.
+    /// </summary>
     interface IChange_Option_Command
     {
         void Change_Option(Options options);
