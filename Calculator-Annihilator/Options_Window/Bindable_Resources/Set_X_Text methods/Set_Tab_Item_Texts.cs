35fc73b09e478110f70711652548f74a1df95bb2
Ungouge <zlobik89@gmail.com>
2019-02-16 00:10:49 +0100

    Added Tab_Items language lisrary and implemented in Set_Texts.

---
+++ Calculator-Annihilator/Options_Window/Bindable_Resources/Set_X_Text methods/Set_Tab_Item_Texts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator_Annihilator
+{
+    public partial class Options_Window
+    {
+        partial class Bindable_Resources
+        {
+            public string Options { get; set; }
+            public string General_Options { get; set; }
+            public string Calculator_Options { get; set; }
+
+            private void Set_Tab_Item_Texts(Standard_Messages standard_Messages)
+            {
+                Options= standard_Messages.Translate("");
+                General_Options = standard_Messages.Translate("");
+                Calculator_Options = standard_Messages.Translate("");
+
+                On_Properties_Change(new string[]
+                {
+                    "Options",
+                    "General_Options",
+                    "Calculator_Options"
+                });
+            }
+        }
+    }
+}
--- Calculator-Annihilator/Options_Window/Bindable_Resources/Set_X_Text methods/Set_Text.cs
+++ Calculator-Annihilator/Options_Window/Bindable_Resources/Set_X_Text methods/Set_Text.cs
@@ -20,6 +20,8 @@ namespace Calculator_Annihilator
         {
             internal void Set_Text(Standard_Messages standard_Messages)
             {
+                Set_Tab_Item_Texts(standard_Messages);
+
                 Set_General_Options_Texts(standard_Messages);
 
                 Set_Buttons_Texts(standard_Messages);
