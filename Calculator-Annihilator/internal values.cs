054c681ca3e173f5257cb2d6b635f7c176dcccab
Ungouge <zlobik89@gmail.com>
2019-02-20 22:37:13 +0100

    Added internal values file with Used_Numeral_Systems:
    With currently used numeral systems.

---
+++ Calculator-Annihilator/internal values.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator_Annihilator
+{
+    internal static class Used_Numeral_Systems
+    {
+        internal static sbyte[][] Numeral_Systems
+        {
+            get { return new sbyte[][] { new sbyte[] { -3, -2 }, new sbyte[] { 1, 64 } }; }
+        }
+    }  
+}
