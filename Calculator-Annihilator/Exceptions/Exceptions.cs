9d8b7b94f523405e74ef24a4bec2ca7156f61870
Ungouge <zlobik89@gmail.com>
2019-02-26 16:40:05 +0100

    Transfered Exceptions to Common_Library.

--- Calculator-Annihilator/Exceptions/Exceptions.cs
+++ Calculator-Annihilator/Exceptions/Exceptions.cs
@@ -44,23 +44,4 @@ namespace Calculator_Annihilator
 
 		}
 	}
-
-	/// <summary>
-	/// The exception that is thrown when is given wrong code number of numerical system.
-	/// </summary>
-	public class NoSuchNumericalSystemException : Exception
-	{
-		public NoSuchNumericalSystemException()
-		{
-
-		}
-		public NoSuchNumericalSystemException(string message) : base(message)
-		{
-
-		}
-		public NoSuchNumericalSystemException(string message, Exception inner) : base(message, inner)
-		{
-
-		}
-	}
 }
---
+++ Common_Library/Exceptions/NoSuchNumericalSystemException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Common_Library
+{
+    /// <summary>
+    /// The exception that is thrown when is given wrong code number of numerical system.
+    /// </summary>
+    public class NoSuchNumericalSystemException : Exception
+    {
+        public NoSuchNumericalSystemException()
+        {
+
+        }
+        public NoSuchNumericalSystemException(string message) : base(message)
+        {
+
+        }
+        public NoSuchNumericalSystemException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
