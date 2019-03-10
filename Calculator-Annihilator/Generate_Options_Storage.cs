2f6fa9e1094000ac10cdf5cb617bdae3ad4895e4
Ungouge <zlobik89@gmail.com>
2019-03-10 01:42:04 +0100

    Fixing destroyed file structure step 1.

---
+++ Calculator-Annihilator/Generate_Options_Storage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Common_Library;
+
+namespace Calculator_Annihilator
+{
+    partial class Options
+    {
+        internal Options_Storage Generate_Options_Storage()
+        {
+            return new Options_Storage(Calculator_Mode, Calculation_Method, Initial_Height, Initial_Width,
+                Number_Notation, Comma_Type, Font_Size_Type, Numeral_System_Code, Pined_Numeral_Systems, Current_Language);
+        }
+    }
+}
---
+++ Calculator-Annihilator/Set_Number_Notation_Type.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Common_Library;
+
+namespace Calculator_Annihilator
+{
+    partial class Options
+    {
+        public void Set_Number_Notation(Number_Notation new_Number_Notation)
+        {
+            Number_Notation = new_Number_Notation;
+        }
+    }
+}
