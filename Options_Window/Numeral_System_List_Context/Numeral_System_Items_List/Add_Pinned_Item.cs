4a5479665870860de9c7c041e5d4add85a8b8051
Ungouge <zlobik89@gmail.com>
2019-03-20 15:15:08 +0100

    Added Pin_Numeral_System_Click and applied.

---
+++ Options_Window/Numeral_System_List_Context/Numeral_System_Items_List/Add_Pinned_Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Options_Library;
+
+namespace Options_Window
+{
+    partial class Numeral_System_List_Context
+    {
+        partial class Numeral_System_Items_List
+        {
+            internal void Add_Pinned_Item(sbyte numeral_System_Code)
+            {
+                Item_Numeral_System_List_Control New_Item = Factory.Get_Item(numeral_System_Code);
+
+                if (Pinned_Items_List.Count > 0)
+                {
+                    int new_Item_Position = 0;
+
+                    for (; new_Item_Position < Pinned_Items_List.Count; new_Item_Position++)
+                        if (Pinned_Items_List[new_Item_Position].Numeral_System_Code > numeral_System_Code)
+                            break;
+
+                    Pinned_Items_List.Insert(new_Item_Position, New_Item);
+                }
+                else
+                    Pinned_Items_List = new List<Numeral_System_List_Item>() { New_Item };
+            }
+        }
+    }
+}
---
+++ Options_Window/Numeral_System_List_Context/Unpin_Numeral_System.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Options_Library;
+
+namespace Options_Window
+{
+    partial class Numeral_System_List_Context
+    {
+        internal void Unpin_Numeral_System(sbyte numeral_System_Code)
+        {
+            Pinned_Numeral_Systems.Remove_Numeral_System(numeral_System_Code);
+
+            _Numeral_System_List_Items_List.Remove_Pinned_Item(numeral_System_Code);
+            
+            _Numeral_System_List_Items_List.Reload_Item(numeral_System_Code);
+
+            On_Property_Change("Numeral_System_List_Items_List");
+        }
+    }
+}
