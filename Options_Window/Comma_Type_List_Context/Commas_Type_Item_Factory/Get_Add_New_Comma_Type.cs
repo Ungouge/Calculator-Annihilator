10302dfd065e637de2f086daf3d0c640f5b5fb01
Ungouge <zlobik89@gmail.com>
2019-03-22 20:54:44 +0100

    Added Comma_Type_List_Content with two parents UserControls

---
+++ Options_Window/Comma_Type_List_Context/Commas_Type_Item_Factory/Get_Add_New_Comma_Type.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Options_Window
+{
+    partial class Commas_Type_Item_Factory
+    {
+        internal Add_New_Comma_Type_Control Get_Add_New_Comma_Type()
+        {
+            return new Add_New_Comma_Type_Control(OptionsWindow);
+        }
+    }
+}
