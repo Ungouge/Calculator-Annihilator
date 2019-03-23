f9898e793c81b099608310388f1caf0ff1a914db
Ungouge <zlobik89@gmail.com>
2019-03-23 11:45:32 +0100

    Added IOptionsWindow_Parameters.

--- IOptions_Window/IOptionsWindow_Parameters.cs
+++ IOptions_Window/IOptionsWindow_Parameters.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using Common_Library;
+using Options_Library;
 
 namespace IOptions_Window
 {
     public interface IOptionsWindow_Parameters
     {
         IStandard_Messages_Translate Get_Standard_Messages { get; }
+
+        IOptions_Provider Get_Options_Provider { get; }
+
+        void Add_New_Comma_Type_Option_Command(string[] Commas_Type);
     }
 }
