216b7882171533ee06161436ad4938e717eb7428
Ungouge <zlobik89@gmail.com>
2019-03-28 23:47:09 +0100

    Added project Starter;
    -Seted Starter as startUp project.

---
+++ Starter/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Starter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            bool Do_load_Hello_Window = false;
+
+            Window Window_To_Load;
+
+            if (Do_load_Hello_Window == false)
+            {
+                Window_To_Load = new Calculator_Annihilator.MainWindow();
+            }
+            else
+            {
+                Window_To_Load = null; // Hello_Window();
+            }
+
+            Application Annihilation_Application = new Application();
+
+            Annihilation_Application.Run(Window_To_Load);
+        }
+    }
+}
