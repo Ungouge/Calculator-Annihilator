0122441f341a4baf59fd984514a0cfd9939dd473
Ungouge <zlobik89@gmail.com>
2019-03-29 17:08:30 +0100

    Moved and renamed Options_Generator to Starter_Library as Get_Options_Storage.

---
+++ Starter_Library/Settings_File_Reader/Get_Options_Storage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Common_Library;
+using Options_Library;
+
+namespace Starter_Library
+{
+    /// <summary>
+    /// Class to read options file or generate new if there is non.
+    /// </summary>
+    public partial class Settings_File_Reader
+    {
+        public IOptions_Storage Get_Options_Storage
+        {
+            get
+            {
+                return Options;
+            }
+        }
+    }
+
+}
