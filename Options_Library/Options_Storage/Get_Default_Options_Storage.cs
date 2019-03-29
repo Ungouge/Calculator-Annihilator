3d82ddda8f56655a77a1e3c8ee2994940dd53082
Ungouge <zlobik89@gmail.com>
2019-03-29 17:09:07 +0100

    Added Get_Default_Options_Storage.

---
+++ Options_Library/Options_Storage/Get_Default_Options_Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Common_Library;
+
+namespace Options_Library
+{
+    public partial class Options_Storage
+    {
+        public static IOptions_Storage Get_Default_Options_Storage
+        {
+            get
+            {
+                return new Options_Storage(
+                    Calculator_Mode.Standard,
+                    Calculation_Method.Single,
+                    510,
+                    420,
+                    Number_Notation.Normal,
+                    System.Globalization.NumberFormatInfo.CurrentInfo.NumberDecimalSeparator,
+                    Font_Size_Type.Normal,
+                    10,
+                    Languages.English
+                );
+            }
+        }
+    }
+}
