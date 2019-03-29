0b37b37a72bbb6b0725bfd3c2c52179c419b180b
Ungouge <zlobik89@gmail.com>
2019-03-29 20:36:54 +0100

    Added Settings_Adpater.

---
+++ Starter_Library/Settings_Adapter/Settings_Adapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starter_Library
+{
+    public class Settings_Adapter: ISettings_Adapter
+    {
+        readonly ApplicationSettingsBase Settings;
+
+        public Settings_Adapter(ApplicationSettingsBase _Settings)
+        {
+            Settings = _Settings;
+        }
+
+        public object this[string propertyName]
+        {
+            get
+            {
+                return Settings[propertyName];
+            }
+        }
+    }
+}
