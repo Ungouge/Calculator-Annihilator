1743b7b02f312ebe48d155fab4e63647074f7788
Ungouge <zlobik89@gmail.com>
2019-03-29 23:26:36 +0100

    Added Start_Up_Window_Setter.

---
+++ Starter_Library/Start_Up_Window_Setter/Set_Start_Up_Window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Start_Up_Window;
+using Common_Library;
+
+namespace Starter_Library
+{
+    partial class Start_Up_Window_Setter
+    {
+        private IStart_Up_Window Set_Start_Up_Window(bool Do_load_Hello_Window)
+        {
+            IStart_Up_Window Window_To_Load;
+
+            if (Do_load_Hello_Window == false)
+            {
+                Window_To_Load = new MainWindow_Factory(Options).Get_MainWindow;
+            }
+            else
+            {
+                Window_To_Load = null; // Hello_Window();
+            }
+
+            return Window_To_Load;
+        }
+    }
+}
---
+++ Starter_Library/Start_Up_Window_Setter/Start_Up_Window_Setter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Options_Library;
+
+namespace Starter_Library
+{
+    internal partial class Start_Up_Window_Setter
+    {
+        private readonly IOptions_Storage Options;
+
+        public Start_Up_Window_Setter(IOptions_Storage _Options)
+        {
+            Options = _Options;
+        }
+    }
+}
