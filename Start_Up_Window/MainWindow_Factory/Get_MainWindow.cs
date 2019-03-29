48e79f3840b17212fd829ac67ff46ad8f7750894
Ungouge <zlobik89@gmail.com>
2019-03-29 23:26:10 +0100

    Added MainWindow_Factory.

---
+++ Start_Up_Window/MainWindow_Factory/Get_MainWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Calculator_Annihilator;
+using Common_Library;
+
+namespace Start_Up_Window
+{
+    partial class MainWindow_Factory
+    {
+        public IMainWindow Get_MainWindow
+        {
+            get
+            {
+                return new MainWindow(Options);
+            }
+        }
+    }
+}
