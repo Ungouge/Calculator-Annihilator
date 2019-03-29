c1632c99bd3e9e80d673e38de06e08828fb73a27
Ungouge <zlobik89@gmail.com>
2019-03-29 17:25:38 +0100

    Added Null_Options_Storage.

---
+++ Options_Library/Options_Storage/Null_Options_Storage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Common_Library;
+
+namespace Options_Library
+{
+    public class Null_Options_Storage: IOptions_Storage
+    {
+        public Calculator_Mode Calculator_Mode
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public Calculation_Method Calculation_Method
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public double Initial_Height
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public double Initial_Width
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public Number_Notation Number_Notation
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public Font_Size_Type Font_Size_Type
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public sbyte Numeral_System_Code
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public Languages Current_Language
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public string Comma_Type
+        {
+            get { throw new ArgumentNullException(); }
+        }
+
+        public string[] Commas_Type_Array
+        {
+            get { throw new ArgumentNullException(); }
+        }
+    }
+}
