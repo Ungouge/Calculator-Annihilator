fc5722af4ecaa54a9c11f03cd6912514959c3262
Ungouge <zlobik89@gmail.com>
2019-04-03 00:46:34 +0200

    Added MainWindow_Factory.

---
+++ Calculator-Annihilator/MainWindow_Factory/Get_MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StartUpWindowBase;
+
+namespace Calculator_Annihilator
+{
+    public partial class MainWindow_Factory
+    {
+        public Start_Up_Window_Base Get_MainWindow
+        {
+            get
+            {
+                return new MainWindow(Options_Storage);
+            }
+        }
+    }
+}
---
+++ Calculator-Annihilator/MainWindow_Factory/MainWindow_Factory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Options_Library;
+
+namespace Calculator_Annihilator
+{
+    public partial class MainWindow_Factory
+    {
+        IOptions_Storage Options_Storage;
+
+        public MainWindow_Factory(IOptions_Storage _Options_Storage)
+        {
+            Options_Storage = _Options_Storage;
+        }
+    }
+}
