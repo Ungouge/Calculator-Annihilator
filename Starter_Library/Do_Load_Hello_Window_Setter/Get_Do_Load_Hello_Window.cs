82b2ec261e44a31b9a807de275a858753f3e36eb
Ungouge <zlobik89@gmail.com>
2019-04-03 01:24:34 +0200

    Buxfix.

---
+++ Starter_Library/Do_Load_Hello_Window_Setter/Get_Do_Load_Hello_Window.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Common_Library;
+using Options_Library;
+
+namespace Starter_Library
+{
+    partial class Do_Load_Hello_Window_Setter
+    {
+        public bool Get_Do_Load_Hello_Window
+        {
+            get
+            {
+                if (Options is Null_Options_Storage)
+                    return true;
+
+                if (Load_Hello_Window_Next_Run.Do_Load == true)
+                    return true;
+
+                return false;
+            }
+        }
+
+    }
+}
