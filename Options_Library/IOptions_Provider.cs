34b7ce9c2cab22d6b074aafe68c85c7c6d24fae4
Ungouge <zlobik89@gmail.com>
2019-04-03 14:49:28 +0200

    Restored change in IOptions_Providerar that disappeared.

--- Options_Library/IOptions_Provider.cs
+++ Options_Library/IOptions_Provider.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using Common_Library;
 
 namespace Options_Library
 {
     public interface IOptions_Provider : IOptions_Storage, ISet_Options
     {
+        IStandard_Messages_Translate Get_Standard_Messages_Translate { get; }
     }
 }
