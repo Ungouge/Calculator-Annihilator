4b31cf20f8b276d15977367823c61860658669f2
Ungouge <zlobik89@gmail.com>
2019-05-16 00:00:53 +0200

    Added Set_Element_Style_Delegate

---
+++ Common_Library/Set_Element_Style_Delegate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common_Library
+{
+    public delegate void Set_Element_Style_Delegate(string element_Name);
+}
