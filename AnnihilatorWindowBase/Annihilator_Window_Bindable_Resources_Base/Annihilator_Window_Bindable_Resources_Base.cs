f614c3bc6929b699148af244679777f88d2d336d
Ungouge <zlobik89@gmail.com>
2019-05-23 00:51:48 +0200

    Added and implemented Set_Font_Size_Type.

--- AnnihilatorWindowBase/Annihilator_Window_Bindable_Resources_Base/Annihilator_Window_Bindable_Resources_Base.cs
+++ AnnihilatorWindowBase/Annihilator_Window_Bindable_Resources_Base/Annihilator_Window_Bindable_Resources_Base.cs
@@ -5,27 +5,13 @@ namespace AnnihilatorWindowBase
 {
     public abstract partial class Annihilator_Window_Bindable_Resources_Base
     {
-        public Annihilator_Window_Bindable_Resources_Base(Font_Size_Type _Font_Size_Type)
+        public Annihilator_Window_Bindable_Resources_Base(Font_Size_Type _Font_Size_Type, Colour_Palette_Type _Colour_Palette_Type)
         {
-            Font_Size_Type = Font_Size_Type.Hight_Contrast;// _Font_Size_Type;
+            Font_Size_Type = _Font_Size_Type;
 
-            switch (Font_Size_Type)
-            {
-                case Font_Size_Type.Small:
-                    Set_Small_Font_Size_Type();
-                    break;
-                case Font_Size_Type.Normal:
-                    Set_Normal_Font_Size_Type();
-                    break;
-                case Font_Size_Type.Big:
-                    Set_Big_Font_Size_Type();
-                    break;
-                case Font_Size_Type.Hight_Contrast:
-                    Set_High_Contrast_Font_Size_Type();
-                    break;
-                default:
-                    goto case Font_Size_Type.Normal;
-            }
+            Colour_Palette_Type = _Colour_Palette_Type;
+
+            Set_Font_Size_Type();
         }
     }
 }
---
+++ AnnihilatorWindowBase/Annihilator_Window_Bindable_Resources_Base/Set_X_Font_Size_Type/Set_Font_Size_Type.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Common_Library;
+
+namespace AnnihilatorWindowBase
+{
+    partial class Annihilator_Window_Bindable_Resources_Base
+    {
+        public void Set_Font_Size_Type()
+        {
+            switch (Font_Size_Type)
+            {
+                case Font_Size_Type.Small:
+                    Set_Small_Font_Size_Type();
+                    break;
+                case Font_Size_Type.Normal:
+                    Set_Normal_Font_Size_Type();
+                    break;
+                case Font_Size_Type.Big:
+                    Set_Big_Font_Size_Type();
+                    break;
+                case Font_Size_Type.Hight_Contrast:
+                    Set_High_Contrast_Font_Size_Type();
+                    break;
+                default:
+                    goto case Font_Size_Type.Normal;
+            }
+        }
+    }
+}
