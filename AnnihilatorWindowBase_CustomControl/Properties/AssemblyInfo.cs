2252d316c25cd9b844261f6b9910a01239eec230
Ungouge <zlobik89@gmail.com>
2019-06-24 15:30:13 +0200

    Renamed AnnihilatorWindowBase_ResourceDictionaries from AnnihilatorWindowBase_CustomControl.

--- AnnihilatorWindowBase_CustomControl/Properties/AssemblyInfo.cs
+++ AnnihilatorWindowBase_CustomControl/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AnnihilatorWindowBase_CustomControl")]
+[assembly: AssemblyTitle("AnnihilatorWindowBase_ResourceDictionaries")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AnnihilatorWindowBase_CustomControl")]
+[assembly: AssemblyProduct("AnnihilatorWindowBase_ResourceDictionaries")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
