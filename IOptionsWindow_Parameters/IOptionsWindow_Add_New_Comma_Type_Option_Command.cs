85bebf8d34a092c68dce88ca173b7c3dbf30016c
Ungouge <zlobik89@gmail.com>
2019-06-28 16:09:56 +0200

    Removed IOptionsWindow_Parameters because it is obsolete.

--- IOptionsWindow_Parameters/IOptionsWindow_Add_New_Comma_Type_Option_Command.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace IOptionsWindow_Parameters
-{
-    public interface IOptionsWindow_Add_New_Comma_Type_Option_Command
-    {
-        void Add_New_Comma_Type_Option_Command(string[] Commas_Type);
-    }
-}
--- IOptionsWindow_Parameters/IOptionsWindow_For_Comma_Type_List_Context.cs
+++ IOptionsWindow_Parameters/IOptionsWindow_For_Comma_Type_List_Context.cs
@@ -3,7 +3,7 @@ using Common_Library;
 
 namespace IOptionsWindow_Parameters
 {
-    public interface IOptionsWindow_For_Comma_Type_List_Context: IOptionsWindow_Add_New_Comma_Type_Option_Command, IOptionsWindow_Get_Options_Storage, IGet_Standard_Messages
+    public interface IOptionsWindow_For_Comma_Type_List_Context: IOptionsWindow_Get_Options_Storage, IGet_Standard_Messages
     {
     }
 }
