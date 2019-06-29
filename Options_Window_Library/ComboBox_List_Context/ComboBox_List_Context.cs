f1503b891a14fb5a202268a918eb2aa0e5d8d1e9
Ungouge <zlobik89@gmail.com>
2019-06-29 11:04:15 +0200

    Added ComboBox_List_Context.

--- Options_Window_Library/ComboBox_List_Context/ComboBox_List_Context.cs
+++ Options_Window_Library/ComboBox_List_Context/ComboBox_List_Context.cs
@@ -3,7 +3,7 @@ using Common_Library;
 
 namespace Options_Window_Library
 {
-    internal abstract class ComboBox_List_Context
+    internal abstract class ComboBox_List_Contextd
     {
         internal abstract void Set_Item_Text(IStandard_Messages_Translate Standard_Messages);
     }
