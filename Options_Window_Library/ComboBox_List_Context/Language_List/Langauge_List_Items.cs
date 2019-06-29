7a5214ef2ee4b67008a8454d1fb8fdcd2c3b0081
Ungouge <zlobik89@gmail.com>
2019-06-29 02:35:24 +0200

    Moved /Language_List to /ComboBox_List_Context.

---
+++ Options_Window_Library/ComboBox_List_Context/Language_List/Langauge_List_Items.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Common_Library;
+
+namespace Options_Window_Library
+{
+    /// <summary>
+    /// Item with resources for Language_List_ComboBox.
+    /// </summary>
+    public class Langauge_List_Items
+    {
+        public string Language_Name { get; private set; }
+
+        public string Language_Flag_Uri { get; private set; }
+
+        internal Languages Language;
+
+        /// <summary>
+        /// Constructs item for given Language.
+        /// </summary>
+        internal Langauge_List_Items(IStandard_Messages_Translate Standard_Messages, Languages _Language)
+        {
+            Language = _Language;
+
+            Language_Name = Standard_Messages.Translate(Language.ToString() + "_Native");
+
+            Language_Flag_Uri = AppDomain.CurrentDomain.BaseDirectory + @"Library\Images\Language_Flags\" + Language.ToString() + "_Flag.png";
+        }
+    }
+}
---
+++ Options_Window_Library/ComboBox_List_Context/Language_List/Set_Languge_List_Items.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Common_Library;
+
+namespace Options_Window_Library
+{
+    partial class Langauge_List_Context
+    {
+        /// <summary>
+        /// Sets list of Language_List_Content.
+        /// </summary>
+        private List<Langauge_List_Items> Set_Languge_List_Items()
+        {
+            List<Langauge_List_Items> Output_Items_List = new List<Langauge_List_Items>();
+
+            foreach (Languages language in (Languages[]) Enum.GetValues(typeof(Languages)))
+            {
+                Output_Items_List.Add( new Langauge_List_Items(Standard_Messages, language));
+            }
+
+            return Output_Items_List;
+        }
+    }
+}
