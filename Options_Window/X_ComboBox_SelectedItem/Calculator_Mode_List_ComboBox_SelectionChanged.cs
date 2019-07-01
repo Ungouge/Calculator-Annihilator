675262ebce814043e6ddd54755537a9f87532eb1
Ungouge <zlobik89@gmail.com>
2019-07-01 21:52:51 +0200

    Dded Calculator_Mode_List_ComboBox in OptionsWindow.

---
+++ Options_Window/X_ComboBox_SelectedItem/Calculator_Mode_List_ComboBox_SelectionChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Controls;
+using Common_Library;
+using Options_Library;
+
+namespace Options_Window
+{
+    partial class OptionsWindow
+    {
+        private void Calculator_Mode_List_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
