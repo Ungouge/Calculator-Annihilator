f9150cbe94bbbbca486ed1c41065f0c227a426ad
Ungouge <zlobik89@gmail.com>
2019-07-01 21:49:30 +0200

    Added Calculator_Mode_List_Item.

---
+++ Options_Window_Library/ComboBox_List_Context/Calculator_Mode_List/Calculator_Mode_List_Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Common_Library;
+
+namespace Options_Window_Library
+{
+    /// <summary>
+    /// Item with resources for Calculator_Mode_List_ComboBox.
+    /// </summary>
+    internal class Calculator_Mode_List_Item : ICalculator_Mode_List_Item
+    {
+        internal Calculator_Mode Calculator_Mode;
+
+        public string Calculator_Mode_Name { get; set; }
+
+        /// <summary>
+        /// Constructs item for given Calculator_Mode.
+        /// </summary>
+        internal Calculator_Mode_List_Item(IStandard_Messages_Translate Standard_Messages, Calculator_Mode _Calculator_Mode)
+        {
+            Calculator_Mode = _Calculator_Mode;
+
+            Calculator_Mode_Name = Standard_Messages.Translate(Calculator_Mode.ToString());
+        }
+    }
+}
---
+++ Options_Window_Library/ComboBox_List_Context/Calculator_Mode_List/ICalculator_Mode_List_Item.cs
@@ -0,0 +1,7 @@
+﻿namespace Options_Window_Library
+{
+    public interface ICalculator_Mode_List_Item : IComboBox_Item_Base
+    {
+        string Calculator_Mode_Name { get; }
+    }
+}
