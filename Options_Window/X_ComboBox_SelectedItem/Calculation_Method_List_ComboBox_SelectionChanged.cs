247249e6b8c1d82f8408426dcf977316f3a9dcad
Ungouge <zlobik89@gmail.com>
2019-07-02 11:45:17 +0200

    Added Calculation_Method_List_ComboBox_SelectionChanged.

---
+++ Options_Window/X_ComboBox_SelectedItem/Calculation_Method_List_ComboBox_SelectionChanged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+using Common_Library;
+using Options_Library;
+
+namespace Options_Window
+{
+    partial class OptionsWindow
+    {
+        private void Calculation_Method_List_ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Remove_Obsolete_Command<Change_Calculation_Method_Option_Command>();
+
+            Calculation_Method New_Calculation_Method = (Calculation_Method)Calculation_Method_List_ComboBox.SelectedIndex;
+
+            if (New_Calculation_Method != Options_Provider.Calculation_Method)
+                Option_Change_Command_List.Add(new Change_Calculation_Method_Option_Command(New_Calculation_Method));
+        }
+    }
+}
