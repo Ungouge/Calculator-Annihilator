26cc3b37d37b2cdf8fe547d3adc91a35ea3e1e8e
Ungouge <zlobik89@gmail.com>
2019-07-02 10:40:31 +0200

    Added Calculation_Method_List_Item.

---
+++ Options_Window_Library/ComboBox_List_Context/Calculation_Method_List/Calculation_Method_List_Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Common_Library;
+
+namespace Options_Window_Library
+{
+    /// <summary>
+    /// Item with resources for Calculation_Method_List_ComboBox.
+    /// </summary>
+    internal class Calculation_Method_List_Item : ICalculation_Method_List_Item
+    {
+        internal Calculation_Method Calculation_Method;
+
+        public string Calculation_Method_Name { get; set; }
+
+        /// <summary>
+        /// Constructs item for given Calculation_Method.
+        /// </summary>
+        internal Calculation_Method_List_Item(IStandard_Messages_Translate Standard_Messages, Calculation_Method _Calculation_Method)
+        {
+            Calculation_Method = _Calculation_Method;
+
+            Calculation_Method_Name = Standard_Messages.Translate(Calculation_Method.ToString());
+        }
+    }
+}
---
+++ Options_Window_Library/ComboBox_List_Context/Calculation_Method_List/ICalculation_Method_List_Item.cs
@@ -0,0 +1,7 @@
+﻿namespace Options_Window_Library
+{
+    public interface ICalculation_Method_List_Item : IComboBox_Item_Base
+    {
+        string Calculation_Method_Name { get; }
+    }
+}
