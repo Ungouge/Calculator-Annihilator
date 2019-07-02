30173c7f7bfd7c29c72688f2339f211f0ccc3a61
Ungouge <zlobik89@gmail.com>
2019-07-02 10:18:51 +0200

    Added Calculator_Mode_List_ComboBox_SelectionChanged.

--- Options_Window/X_ComboBox_SelectedItem/Calculator_Mode_List_ComboBox_SelectionChanged.cs
+++ Options_Window/X_ComboBox_SelectedItem/Calculator_Mode_List_ComboBox_SelectionChanged.cs
@@ -9,7 +9,12 @@ namespace Options_Window
     {
         private void Calculator_Mode_List_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Remove_Obsolete_Command<Change_Calculator_Mode_Option_Command>();
 
+            Calculator_Mode New_Calculator_Mode = (Calculator_Mode)Calculator_Mode_List_ComboBox.SelectedIndex;
+
+            if (New_Calculator_Mode != Options_Provider.Calculator_Mode)
+                Option_Change_Command_List.Add(new Change_Calculator_Mode_Option_Command(New_Calculator_Mode));
         }
     }
 }
